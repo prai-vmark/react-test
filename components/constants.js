e1ea76979af3edd0a1f5e42b40e1a32d3f83813f
Pravin Rai <prai@vestmark.com>
2018-01-31 10:37:09 -0500

    Updates

--- components/constants.js
+++
@@ -1,7 +0,0 @@
-const Constants = {
-  baseUrl: "https://www.cryptocompare.com",
-  dataUrl: "https://min-api.cryptocompare.com/data/subs?",
-  streamUrl: "https://streamer.cryptocompare.com/"
-};
-
-export default Constants;
